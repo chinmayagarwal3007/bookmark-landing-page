8efe97a60e06c19afebb51c4da00087d8c60ca4a
Chinmay <agarwalchinmay3007@gmail.com>
2024-01-06 11:26:47 +0530

    woking on Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-
 import styles from "./navbar.module.css"
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.container}>
+
+      <div className={styles.image}>
+          <Image src="/logo-bookmark.png" alt='logo' width={148} height={25} className={styles.logo}/> 
+      </div>
+
+      <div className={styles.links}>
+        <p className={styles.link}>FEATURES</p>
+        <p className={styles.link}>PRICING</p>
+        <p className={styles.link}>CONTACT</p>
+        <button className={styles.button}>LOGIN</button>
+      </div>
+
+      <div className={styles.hamburger}>
+        <Image src="/icon-hamburger.png" alt='hamburger' width={18} height={15}   /> 
+      </div>
+    </div>
   )
 }
 
