8478112e6fc487dc5ee9b902584aa31e0178c6ac
Chinmay <agarwalchinmay3007@gmail.com>
2024-01-25 19:24:37 +0530

    Day 9

--- src/components/addBox/AddBox.jsx
+++ src/components/addBox/AddBox.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
+import styles from "./addBox.module.css"
 
 const AddBox = ({ imageSrc, browserName, version, height, width }) => {
   return (
-    <div>
-        <Image src={imageSrc} alt='browserImg' height={height} width={width} />
-        <p>Add to {browserName}</p>
+    <div className={styles.container}>
+        <Image src={imageSrc} alt='browserImg' height={height} width={width} className={styles.img}/>
+        <h1>Add to {browserName}</h1>
         <p>Minimum version {version}</p>
+        <hr className={styles.line} />
+        <button className={styles.btn}>Add & Install Extension</button>
     </div>
   )
 }
