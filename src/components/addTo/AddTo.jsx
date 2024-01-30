add7aa8c6bffd87bf657c03a6df9bc2da0a3c23a
Chinmay <agarwalchinmay3007@gmail.com>
2024-01-30 12:24:00 +0530

    Day 11

--- src/components/addTo/AddTo.jsx
+++ src/components/addTo/AddTo.jsx
@@ -5,14 +5,14 @@ import AddBox from '@/components/addBox/AddBox'
 const AddTo = () => {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.text_container}>
         <h1>Download the extension</h1>
         <p>We've got more browsers in the pipeline. Please do let us know if you've got a favourite you'd like us to prioritize.</p>
       </div>
       <div className={styles.addToContainer}>
         <AddBox imageSrc = "/logo-chrome.png" browserName = "Chrome" version ={62} height={102} width={100} />
-        <AddBox imageSrc = "/logo-firefox.png" browserName = "Firefox" version ={55} height={102} width={100} style={{ marginTop: '300px' }}/>
-        <AddBox imageSrc = "/logo-opera.png" browserName = "Opera" version ={46} height={102} width={100} style = "margin-top:60px"/>
+        <AddBox imageSrc = "/logo-firefox.png" browserName = "Firefox" version ={55} height={102} width={100} />
+        <AddBox imageSrc = "/logo-opera.png" browserName = "Opera" version ={46} height={102} width={100} />
       </div>
 
     </div>
