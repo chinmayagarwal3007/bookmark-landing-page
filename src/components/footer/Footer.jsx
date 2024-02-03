84078a2b31f9bf451efd61148d80c5a32a46dd9f
Chinmay <agarwalchinmay3007@gmail.com>
2024-02-03 10:46:34 +0530

    Finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import Image from 'next/image'
 import styles from "./footer.module.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.tags}>
+      <Image src="/logo-bookmark_light.png" alt='logo' width={148} height={25} />
+      <p>FEATURES</p>
+      <p>PRICING</p>
+      <p>CONTACT</p>
+      </div>
+
+      <div className={styles.social}>
+        <Image src="/icon-facebook.png" alt='facebook' height={24} width={24} />
+        <Image src="/icon-twitter.png" alt='twitter' height={24} width={24} />
+      </div>
+    </div>
   )
 }
 
