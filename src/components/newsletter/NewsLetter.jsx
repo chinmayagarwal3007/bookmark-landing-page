42cdb9cdf77139468dce1c24ce176ba4eb2873fc
Chinmay <agarwalchinmay3007@gmail.com>
2024-02-03 10:56:12 +0530

    Typo corrected

--- src/components/newsletter/NewsLetter.jsx
+++ src/components/newsletter/NewsLetter.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import styles from "./newsLetter.module.css";
+import styles from "./newsletter.module.css";
 import Image from "next/image";
 
 import { useState } from "react";
